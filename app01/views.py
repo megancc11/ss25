eda700acb4139cdc18fecd41944364aa9be6563e
Megan <276324046@qq.com>
2024-02-27 17:55:26 +0800

    修改发送短信使用的模板

--- app01/views.py
+++ app01/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render,HttpResponse
 from utils.tencent.sms import send_sms_single
 import random
-# Create your views here.
+from django.conf import settings
+
 def send_sms(request):
-    """发送短信"""
+    """发送短信
+    ?tpl=login ->1111
+    ?tpl=register ->2222
+    """
+    tpl=request.GET.get('tpl')
+    template_id=settings.TENCENT_SMS_TEMPLATE.get(tpl)
+    if not template_id:
+        return HttpResponse('模板不存在')
+
     code=random.randrange(1000,9999)
-    res=send_sms_single('135552','548760',[code,])
+    res=send_sms_single('135552',template_id,[code,])
     print(res)
     if res['result']==0:
         return HttpResponse('发送成功')
